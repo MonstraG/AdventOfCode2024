967462c3f90d757e867258c85bb17dfeabbe7932
Arseny Garelyshev <monstrag@gmail.com>
2024-12-04 15:04:20 +0100

    found the problem

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Advent of code 2024
 
 Me, doing advent of code until I get bored
+
+It seems that there is quite a lot of opportunity for optimisation,
+like not reading the entire file at once, but for me, the puzzle solutions run
+instantly anyway so I don't bother for now
--- two/2_test.go
+++ two/2_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// TestIsDampenedReportValid covers test cases from the task
 func TestIsDampenedReportValid(t *testing.T) {
 	reports := []struct {
 		report []int
@@ -16,6 +17,28 @@ func TestIsDampenedReportValid(t *testing.T) {
 		{[]int{1, 3, 2, 4, 5}, true},
 		{[]int{8, 6, 4, 4, 1}, true},
 		{[]int{1, 3, 6, 7, 9}, true},
+	}
+	for index, tt := range reports {
+		t.Run(strconv.Itoa(index), func(t *testing.T) {
+			got := isDampenedReportValid(tt.report)
+			if got != tt.valid {
+				t.Errorf("got %v, want %v", got, tt.valid)
+			}
+		})
+	}
+}
+
+// TestIsDampenedReportValid2 covers test cases I made up myself
+func TestIsDampenedReportValid2(t *testing.T) {
+	reports := []struct {
+		report []int
+		valid  bool
+	}{
+		// should be true: first 3 can be removed.
+		// at the time of writing the test, this fails because my code
+		// assumses that first pair defines direction for the entire run,
+		// and removes first found failure which would be 2, and is incorrect
+		{[]int{3, 1, 2, 3}, true},
 	}
 	for index, tt := range reports {
 		t.Run(strconv.Itoa(index), func(t *testing.T) {
