952936b016f1ea6ed06070ea0b881290dada9761
Arseny Garelyshev <monstrag@gmail.com>
2024-12-04 13:19:09 +0100

    day two puzzle one done

--- two/1.go
+++ two/1.go
@@ -8,13 +8,64 @@ import (
 	"strings"
 )
 
+func sameSign(a, b int) bool {
+	return a^b >= 0
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func isDifferenceValid(expectedDirection int, this int) bool {
+	if this == 0 {
+		return false
+	}
+	if !sameSign(expectedDirection, this) {
+		return false
+	}
+	if abs(this) > 3 {
+		return false
+	}
+	return true
+}
+
+func isReportValid(report []int) bool {
+	if len(report) < 2 {
+		// this is unspecified, but I'll assume that report of 1 number or less is invalid
+		return false
+	}
+
+	previous := report[0]
+
+	// take first difference as the canonical for report
+	reportDiff := report[0] - report[1]
+	if !isDifferenceValid(reportDiff, reportDiff) {
+		return false
+	}
+
+	for _, level := range report[1:] {
+		levelDiff := previous - level
+		if !isDifferenceValid(reportDiff, levelDiff) {
+			return false
+		}
+
+		previous = level
+	}
+
+	return true
+}
+
 func countValidReports(reports [][]int) int {
 	totalValid := 0
-	for range reports {
-
+	for _, report := range reports {
+		if isReportValid(report) {
+			totalValid += 1
+		}
 	}
 	return totalValid
-
 }
 
 func parseLines(lines []string) [][]int {
---
+++ two/1_test.go
@@ -0,0 +1,28 @@
+package two
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestIsReportValid(t *testing.T) {
+	reports := []struct {
+		report []int
+		valid  bool
+	}{
+		{[]int{7, 6, 4, 2, 1}, true},
+		{[]int{1, 2, 7, 8, 9}, false},
+		{[]int{9, 7, 6, 2, 1}, false},
+		{[]int{1, 3, 2, 4, 5}, false},
+		{[]int{8, 6, 4, 4, 1}, false},
+		{[]int{1, 3, 6, 7, 9}, true},
+	}
+	for index, tt := range reports {
+		t.Run(strconv.Itoa(index), func(t *testing.T) {
+			got := isReportValid(tt.report)
+			if got != tt.valid {
+				t.Errorf("got %v, want %v", got, tt.valid)
+			}
+		})
+	}
+}
